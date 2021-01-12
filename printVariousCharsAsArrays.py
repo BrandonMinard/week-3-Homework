a77f31c70941994d319431e2709fe2c9b7c98df3
Brandon Laptop <brandon.minard@gmail.com>
2021-01-11 17:21:03 -0700

    added comments to python script

--- printVariousCharsAsArrays.py
+++ printVariousCharsAsArrays.py
@@ -1,14 +1,32 @@
+#python lists are formatted the same as js arrays.
+#so using python to just get a bunch of formatted arrays.
+
+#import string library
 import string
+#get lowers as string, cast to list.
 lowers = string.ascii_lowercase
 lowersArr = list(lowers)
+#print list.
 print(lowersArr)
+
+#get uppers as string, cast to list.
 uppers = string.ascii_uppercase
 uppersArr = list(uppers)
+#print list
 print(uppersArr)
+
+#copied from https://owasp.org/www-community/password-special-characters
+#except for ' and " which have to be manually added.
+#cast to list, add ', "
 specials = " !#$%&()*+,-./:;<=>?@[\]^_`{|}~"
 specialsList = list(specials)
 specialsList.append('"')
 specialsList.append("'")
+#print list
 print(specialsList)
-numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+#numbers as string, cast to list.
+numbers = "1234567890"
+numbers = list(numbers)
+#print number list
 print(numbers)
